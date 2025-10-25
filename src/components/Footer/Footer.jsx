852546959ab62cb2759d7cf13a810525358a59d6
Kalcindor <kevin.alcindor@gmail.com>
2025-10-25 23:50:39 +0200

    [feature]: draft footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,3 +1,13 @@
-export default function APropos() {
-    return <h1>À propos</h1>;
-  }
+import "./Footer.scss";
+//import logo from "../../assets/logo-white.svg"; // à ajouter juste en dessous
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      
+      <p className="footer__text">© 2020 Kasa. All rights reserved.</p>
+    </footer>
+  );
+}
+
+//<img src={logo} alt="Kasa" className="footer__logo" />
