f01d6db099294c55d1fda3eca3ec203ced2a1cab
Kalcindor <kevin.alcindor@gmail.com>
2025-10-26 16:46:21 +0100

    [update]: updating global displaying

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,14 @@
 import { NavLink } from "react-router-dom";
+//import logo from "../../assets/logo-red.svg";
 import "./Header.scss";
-
+//<img src={logo} alt="Kasa" className="header__logo" />;
 export default function Header() {
   return (
-    <header>
-      <nav>
-        <NavLink to="/">Accueil</NavLink>
+    <header className="header">
+      <nav className="header__nav">
+        <NavLink to="/" end>
+          Accueil
+        </NavLink>
         <NavLink to="/about">À propos</NavLink>
       </nav>
     </header>
--- src/pages/Housing/Housing.jsx
+++ src/pages/Housing/Housing.jsx
@@ -3,6 +3,7 @@ import data from "../../data/housing.json";
 import Tag from "../../components/Tag/Tag";
 import Rating from "../../components/Rating/Rating";
 import Collapse from "../../components/Collapse/Collapse";
+import Carousel from "../../components/Carousel/Carousel";
 
 import "./Housing.scss";
 
@@ -13,25 +14,43 @@ export default function Housing() {
   if (!housing) return <Navigate to="/not-found" />;
   return (
     <>
-      <h1>{housing.title}</h1>
-      <p>{housing.location}</p>
-      <div className="tags">
-        {housing.tags.map((tag, index) => (
-          <Tag key={index} label={tag} />
-        ))}
-      </div>
-      <div className="rating-container">
-        <Rating value={parseInt(housing.rating)} />
-      </div>
-      <Collapse title="Description">{housing.description}</Collapse>
+      <Carousel pictures={housing.pictures} />
 
-      <Collapse title="Équipements">
-        <ul>
-          {housing.equipments.map((item, index) => (
-            <li key={index}>{item}</li>
-          ))}
-        </ul>
-      </Collapse>
+      <div className="housing-header">
+        <div className="housing-info">
+          <h1>{housing.title}</h1>
+          <p className="housing-location">{housing.location}</p>
+
+          <div className="tags">
+            {housing.tags.map((tag, index) => (
+              <Tag key={index} label={tag} />
+            ))}
+          </div>
+        </div>
+
+        <div className="housing-host-rating">
+          <div className="housing-host">
+            <p className="housing-host__name">{housing.host.name}</p>
+            <img
+              className="housing-host__picture"
+              src={housing.host.picture}
+              alt={housing.host.name}
+            />
+          </div>
+          <Rating value={parseInt(housing.rating)} />
+        </div>
+      </div>
+
+      <div className="housing-collapse">
+        <Collapse title="Description">{housing.description}</Collapse>
+        <Collapse title="Équipements">
+          <ul>
+            {housing.equipments.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
+        </Collapse>
+      </div>
     </>
   );
 }
